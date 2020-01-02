810bcd34a7ce976c5de4fddb75e71bc7ec3c395c
ralexand56 <ralexand56@live.com>
2020-01-02 12:08:58 -0800

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Canvas, useFrame } from "react-three-fiber";
+import React, { useRef, useState } from "react";
+import { useSpring, a } from "react-spring/three";
 
-const App: React.FC = () => {
+const Thing = () => {
+  // const [hovered, setHovered] = useState(false);
+  const [active, setActive] = useState(false);
+  // const [isIncreasing, setIsIncreasing] = useState(true);
+  const props = useSpring({
+    scale: active ? [2, 2, 2] : [1, 1, 1],
+    color: active ? "hotpink" : "gray"
+  });
+  const ref = useRef(null);
+  useFrame(() => {
+    ref.current.rotation.x = ref.current.rotation.y += 0.01;
+    // console.log(ref.current.scale.x);
+    // ref.current.scale.x = ref.current.scale.x += isIncreasing ? 0.01 : -0.01;
+
+    // if (ref.current.scale.x < 1) {
+    //   setIsIncreasing(true);
+    //   ref.current.scale.x = 1;
+    // }
+
+    // if (ref.current.scale.x > 5) {
+    //   setIsIncreasing(false);
+    //   ref.current.scale.x = 5;
+    // }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <a.mesh ref={ref} onClick={() => setActive(!active)} scale={props.scale}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshNormalMaterial attach="material" color={props.color} />
+    </a.mesh>
   );
-}
+};
+
+const App = () => (
+  <>
+    <h1 className="grad">Happy 2020</h1>
+    <Canvas>
+      <Thing />
+    </Canvas>
+  </>
+);
 
 export default App;
